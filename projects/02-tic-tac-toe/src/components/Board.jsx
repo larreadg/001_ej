c2a5c351f719a9a8f2da5716e09000895485b4ed
Diego Larrea <larreadi@personal.com.py>
2024-02-02 16:25:47 -0300

    ADD: standard js

--- projects/02-tic-tac-toe/src/components/Board.jsx
+++ projects/02-tic-tac-toe/src/components/Board.jsx
@@ -1,33 +1,32 @@
-import { TURNS } from "../constants"
-import { Square } from "./Square"
+import { TURNS } from '../constants'
+import { Square } from './Square'
 
-export function Board({ board, updateBoard, turn }) {
-
-    return (
-        <>
-            <section className='game'>
-                {
+export function Board ({ board, updateBoard, turn }) {
+  return (
+    <>
+      <section className='game'>
+        {
                     board.map((_, index) => {
-                        return (
-                        <Square 
-                            key={index}
-                            index={index}
-                            updateBoard={updateBoard}
+                      return (
+                        <Square
+                          key={index}
+                          index={index}
+                          updateBoard={updateBoard}
                         >
-                            {board[index]}
+                          {board[index]}
                         </Square>
-                        )
+                      )
                     })
                 }
-            </section>
-            <section className='turn'>
-                <Square isSelected={turn === TURNS.X}>
-                {TURNS.X}
-                </Square>
-                <Square isSelected={turn === TURNS.O}>
-                {TURNS.O}
-                </Square>
-            </section>
-        </>
-    )
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </>
+  )
 }
